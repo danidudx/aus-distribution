8ce371eee68edfa8ecee1f9b00a7fb4b8c4ab545
Hasan Chinthaka <pgrhasanchinthaka@gmail.com>
2025-05-14 13:53:39 +0530

    update logos.jsx

--- components/features/logos.jsx
+++ components/features/logos.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const logo = [
-  { src: 'assets/images/LOGO1.png', alt: 'jQuery' },
-  { src: 'assets/images/LOGO 2.png', alt: 'Bridgestone' },
-  { src: 'assets/images/LOGO 3.png', alt: 'Dash' },
-  { src: 'assets/images/LOGO 4.png', alt: 'Dunlop' },
-  { src: 'assets/images/LOGO5.png', alt: 'Giant' },
-  { src: 'assets/images/LOGO6.png', alt: 'Digg' },
+  { src: 'assets/images/LOGO 1.png', alt: 'Elite Real Estate Agency Rockhampton' },
+  { src: 'assets/images/LOGO 2.png', alt: 'HHH Accountants' },
+  { src: 'assets/images/LOGO 3.png', alt: 'JUCY Rentals' },
+  { src: 'assets/images/LOGO 4.png', alt: 'Konnect' },
+  { src: 'assets/images/LOGO 5.png', alt: 'LeapIn' },
+  { src: 'assets/images/LOGO 6.png', alt: 'LJ Hooker Rockhampton' },
+  { src: 'assets/images/LOGO 7.png', alt: 'Mr Real Estate Rockhampton' },
+  { src: 'assets/images/LOGO 8.png', alt: "Pat O'Driscoll Real Estate" },
+  { src: 'assets/images/LOGO 9.png', alt: 'Ray White Rockhampton' },
 ];
 
 const Logos = () => {
