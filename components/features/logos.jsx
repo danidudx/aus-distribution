187b906f9a60b28a689438700651d6d5b06350fb
danidudx <projectfiles201@gmail.com>
2025-05-20 10:04:05 +0530

    img name change

--- components/features/logos.jsx
+++ components/features/logos.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const logo = [
-  { src: 'assets/images/LOGO 1.png', alt: 'Elite Real Estate Agency Rockhampton' },
-  { src: 'assets/images/LOGO 2.png', alt: 'HHH Accountants' },
-  { src: 'assets/images/LOGO 3.png', alt: 'JUCY Rentals' },
-  { src: 'assets/images/LOGO 4.png', alt: 'Konnect' },
-  { src: 'assets/images/LOGO 5.png', alt: 'LeapIn' },
-  { src: 'assets/images/LOGO 6.png', alt: 'LJ Hooker Rockhampton' },
-  { src: 'assets/images/LOGO 7.png', alt: 'Mr Real Estate Rockhampton' },
-  { src: 'assets/images/LOGO 8.png', alt: "Pat O'Driscoll Real Estate" },
-  { src: 'assets/images/LOGO 9.png', alt: 'Ray White Rockhampton' },
+  {
+    src: "assets/images/LOGO1.png",
+    alt: "Elite Real Estate Agency Rockhampton",
+  },
+  { src: "assets/images/LOGO2.png", alt: "HHH Accountants" },
+  { src: "assets/images/LOGO3.png", alt: "JUCY Rentals" },
+  { src: "assets/images/LOGO4.png", alt: "Konnect" },
+  { src: "assets/images/LOGO5.png", alt: "LeapIn" },
+  { src: "assets/images/LOGO6.png", alt: "LJ Hooker Rockhampton" },
+  { src: "assets/images/LOGO7.png", alt: "Mr Real Estate Rockhampton" },
+  { src: "assets/images/LOGO8.png", alt: "Pat O'Driscoll Real Estate" },
+  { src: "assets/images/LOGO9.png", alt: "Ray White Rockhampton" },
 ];
 
 const Logos = () => {
